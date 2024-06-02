541fc404ad140c9bc44d3d04d9da452dfcb1f8cf
Zainab Bibi <bzainab501@gmail.com>
2024-06-02 14:16:05 -0700

    commit

--- app.js
+++ app.js
@@ -7,33 +7,174 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-function setItem(itemText) {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText(`View Order ${itemText}!`);
-        item = itemText;
-        tg.MainButton.show();
-    }
-}
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12 = document.getElementById("btn12");
 
-function addButtonListener(btn, itemText) {
-    btn.addEventListener("click", function(){
-        setItem(itemText);
-    });
-}
 
-let buttons = document.querySelectorAll("[id^='btn']");
-
-buttons.forEach((btn, index) => {
-    addButtonListener(btn, index + 1);
+btn1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 4.99$");
+		item = "1";
+		tg.MainButton.show();
+	}
 });
+
+btn2.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 1.49$");
+		item = "2";
+		tg.MainButton.show();
+	}
+});
+
+btn3.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 3.49$");
+		item = "3";
+		tg.MainButton.show();
+	}
+});
+
+btn4.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 3.99$");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 7.99$");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
+
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 1.49$");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+
+
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 1.99$");
+		item = "7";
+		tg.MainButton.show();
+	}
+});
+
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 1.49$");
+		item = "8";
+		tg.MainButton.show();
+	}
+});
+
+btn9.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 10.99$");
+		item = "9";
+		tg.MainButton.show();
+	}
+});
+
+btn10.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 5.99$");
+		item = "10";
+		tg.MainButton.show();
+	}
+});
+
+btn11.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 3.99$");
+		item = "11";
+		tg.MainButton.show();
+	}
+});
+
+
+btn12.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Pay 7.99$");
+		item = "12";
+		tg.MainButton.show();
+	}
+});
+
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(item);
+	tg.sendData(item);
 });
 
+
 let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
 usercard.appendChild(p);
+
+
+
+
+
+
+
+
